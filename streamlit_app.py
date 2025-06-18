023bcf4ec3b9311becad02d74476696dc55e9952
124102627 <Rishsten@gmail.com>
2025-06-18 18:03:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 st.title('Mind Map AI')
-url = 'https://raw.githubusercontent.com/124102627/MindMapAI/master/standardised_mental_health_death_rates_2021.csv'
-df = pd.read_csv(url)
+pd.read_csv('https://raw.githubusercontent.com/124102627/MindMapAI/refs/heads/master/standardised_mental_health_death_rates_2021.csv')
+pd
